d19e1a4af674dc7831acc9f65975eec0dfe231a8
Mio Suzuki <sz.km.io.56@gmail.com>
2013-06-04 14:18:10 +0900

    first commit

---
+++ RecordPerformance.java
@@ -0,0 +1,33 @@
+/**
+ * MIDIキーボードからの入力を記録し，midファイルに出力する
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import javax.sound.midi.*;
+
+/**
+ * MIDIキーボードまわりの設定を行うクラス
+ * @author suzukimio
+ *
+ */
+class RecordPerformance {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
+
+/**
+ * MIDIデバイスからの入力を受けとるクラス
+ * @author suzukimio
+ *
+ */
+class MyReceiver {
+	
+}
